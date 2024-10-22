611acfc8e755dc380b76f5268c8ed74c8caff08e
Laura <140566130+laurabluna@users.noreply.github.com>
2024-10-22 11:14:49 -0300

    Add files via upload

---
+++ ExcecaoCores.java
@@ -0,0 +1,11 @@
+public class PilhaVaziaExcecao extends RuntimeException {
+    public PilhaVaziaExcecao(String err) {
+        super(err);
+    }
+}
+
+public class PilhaCheiaExcecao extends RuntimeException {
+    public PilhaCheiaExcecao(String err) {
+        super(err); 
+    }
+}
