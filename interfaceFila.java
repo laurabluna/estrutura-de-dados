c99fa483ece2d2708d301ef3636614abae710c37
Laura <140566130+laurabluna@users.noreply.github.com>
2024-10-23 10:14:58 -0300

    Add files via upload

---
+++ interfaceFila.java
@@ -0,0 +1,9 @@
+public interface interfaceFila {
+    public void enqueue(); 
+    public Object dequeue();
+    public Object first(); 
+    public int size();
+    public boolean isEmpty(); 
+
+
+}
