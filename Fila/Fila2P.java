b59e0a932999f27c5d3232bb90280f9444cfbd21
Laura <140566130+laurabluna@users.noreply.github.com>
2024-11-11 03:03:08 +0000

    adcionando

---
+++ Fila/Fila2P.java
@@ -0,0 +1,11 @@
+public class Fila2P {
+    private int capacidade;
+    private int topo;
+    private int final;
+    
+
+    public Fila2P(int capacidade) {
+        this.capacidade = capacidade;
+        
+    }
+}
