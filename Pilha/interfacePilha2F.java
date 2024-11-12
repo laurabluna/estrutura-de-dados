f24aeb24a88582850ef813e3066540ab395af4a2
Laura <140566130+laurabluna@users.noreply.github.com>
2024-11-12 14:43:07 +0000

    adcionando

---
+++ Pilha/interfacePilha2F.java
@@ -0,0 +1,10 @@
+public interface Pilha2F {
+    public void push(); 
+    public Object pop();
+    public void trocarFilaF();
+    public void trocarFilaI();
+    public int size();
+    public boolean isEmpty();
+
+
+}
