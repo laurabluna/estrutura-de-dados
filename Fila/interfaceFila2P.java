38d10b1927323cbcdbca6917b6cfc04d03dbd4d1
Laura <140566130+laurabluna@users.noreply.github.com>
2024-11-12 13:59:43 +0000

    adcionando

---
+++ Fila/interfaceFila2P.java
@@ -0,0 +1,12 @@
+public interface Fila2P {
+    public void enqueue();
+    public void aumentaCap(); 
+    public Object dequeue(); 
+    public boolean isEmpty(); 
+    public void trocarPilhaS();
+    public void trocarPilhaE();
+    public int size(); 
+    public void imprimir();
+    public Object First(); 
+
+}
