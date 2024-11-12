964d29ff6c3f80361e476fdf13c3d8ba350c9671
Laura <140566130+laurabluna@users.noreply.github.com>
2024-11-12 12:49:31 +0000

    adcionando

---
+++ Pilha/interfaceCores.java
@@ -0,0 +1,10 @@
+import ExcecaoCores; 
+
+public interface Cores {
+    public PilhasColoridas(int tam); 
+    public void pushV(int valor);
+    public void pushP(int valor);
+    public int popV();
+    public int popP(); 
+
+}
