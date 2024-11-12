a7dc4a5d37c5e8ec278dbfd8aaf2610dc89c6f22
Laura <140566130+laurabluna@users.noreply.github.com>
2024-11-12 18:46:31 +0000

    implementando

---
+++ Pilha/PilhaListaEncadeada.java
@@ -0,0 +1,51 @@
+public class PilhaListaEncadeada {
+    private No inicio;
+    private int tam; 
+
+    public PilhaListaEncadeada() {
+        this.inicio = null;
+        this. tam = 0; 
+    }
+
+    public void push(Object elemento) {
+        No NoNovo = new No(elemento);
+        if(inicio == null) {
+            inicio = NovoNo;
+        }
+        else{
+            No temporario = inicio; 
+            inicio = NovoNo;
+            inicio.prox = temporario;
+        }
+        tam++;
+    }
+
+    public Object pop() {
+        if(isEmpty()) {
+            throw new PilhaVaziaExcecao("essa pilha ta vazia hein!"); 
+        }
+        else{
+            No temporario = inicio; 
+            inicio = inicio.prox;
+            tam--; 
+            return temporario; 
+        }
+    }
+
+    public boolean isEmpty() {
+        return inicio == null;
+    }
+
+    public int size() {
+        return tam; 
+    }
+
+    public Object top() {
+        if(isEmpty()) {
+            throw new PilhaVaziaExcecao("essa pilha tá vazia hein!");
+        }
+        else{
+            return inicio.elemento; 
+        }
+    }
+}
