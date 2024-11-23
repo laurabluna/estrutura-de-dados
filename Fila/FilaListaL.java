f9c7431b69a20c41beefa2efc5be0c7282175fef
Laura <140566130+laurabluna@users.noreply.github.com>
2024-11-23 02:49:07 +0000

    adc

--- Fila/FilaListaL.java
+++ Fila/FilaListaL.java
@@ -3,30 +3,43 @@ public class FilaListaL {
     private No inicio; 
     private No fim;
 
+    private class No {
+        Object elemento;
+        No prox;
+
+        public No(Object elemento) {
+            this.elemento = elemento;
+            this.prox = null;
+        }
+    }
+
     public FilaListaL() {
         this.tamanho = 0; 
         this.inicio = null; 
+        this.fim = null;
     }
 
     public void enqueue(Object elemento) {
-        No NoNovo = new no(elemento); 
+        No novo = new No(elemento); 
         if(inicio == null) {
-            inicio = Nonovo; 
-        } else {
-            fim.prox = NoNovo;
-            fim = Nonovo; 
+            inicio = novo; 
+        } 
+        
+        else {
+            fim.prox = novo;
+            fim = novo; 
         }
         tamanho++; 
     }
 
-    public void dequeue() {
+    public Object dequeue() {
         if(isEmpty()) {
             throw new FilaVaziaExcecao("essa fila ta vazia hein");
         }
-        No Prov = inicio;
+        No prov = inicio;
         inicio = inicio.prox;
         tamanho--; 
-        return No Prov;
+        return prov.elemento;
     }
 
     public int size() {
@@ -48,7 +61,7 @@ public class FilaListaL {
         No atual = inicio; 
         while(atual != null) {
             System.out.println(atual.elemento + " "); 
-            atual = atual + prox;
+            atual = atual.prox;
         }
         System.out.println(); 
     }
